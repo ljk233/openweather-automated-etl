0465d397d0aa73efaa4fefb69b5a6a8b5c4952dd
Liam Kelly <liam.james.kelly@gmail.com>
2024-11-18 03:41:53 +0000

    Added config initialization

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-""""""
+"""main.py"""
+
+from src.config import load_config
+from src.flow import run_flow
+
+
+CONFIG_FILE_PATH = "./config/config.yaml"
+
+
+def main(config_file_path: str):
+    """ """
+    # Load the configuration file
+    config = load_config(config_file_path)
+
+    # Run the pipeline
+    output = run_flow(config)
+
+    print(output)
+
+
+if __name__ == "__main__":
+    main(CONFIG_FILE_PATH)
